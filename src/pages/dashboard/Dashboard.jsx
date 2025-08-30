ce7adf292d3108251e246feab5540f9d5da97b90
Jordhy Branenda <ethgalleryin@gmail.com>
2025-08-30 15:08:50 +0700

    Initial commit

---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard Page OK</h1>
+    </div>
+  );
+}
---
+++ src/pages/landing/Landing.jsx
@@ -0,0 +1,41 @@
+import "../../styles/global.css";
+import "./Landing.css";
+
+export default function Landing() {
+  return (
+    <>
+      {/* Hero Section */}
+      <section className="hero">
+        <div className="hero-container">
+          <h1 className="hero-title">
+            Clear Your Desk, Clear Your Mind.
+            <br />
+            Focus Made Visible.
+          </h1>
+          <button className="hero-btn">Get Started</button>
+        </div>
+      </section>
+
+      <div className="layout">
+
+        {/* Overview Section */}
+        <section className="overview">
+          <div className="overview-container">
+            <h2>Overview</h2>
+            <p>Some intro text here...</p>
+          </div>
+        </section>
+
+        {/* Contact Section */}
+        <section className="contact-us">
+          <div className="contact-us-container">
+            <h2>Contact Us</h2>
+            <p>Email us at: example@email.com</p>
+          </div>
+        </section>
+
+      </div>
+
+    </>
+  );
+}
