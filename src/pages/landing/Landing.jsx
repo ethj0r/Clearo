b9e3fc01b78c603f7090d6634d24427f1498962d
Jordhy Branenda <ethgalleryin@gmail.com>
2025-09-02 21:16:16 +0700

    feat: overview section implementation

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -16,25 +16,61 @@ export default function Landing() {
         </div>
       </section>
 
-      <div className="layout">
+      {/* Overview Section */}
+      <section className="overview">
+        <div className="overview-container">
+          <h2 className="overview-title">Overview</h2>
+          <p className="overview-text">
+            <span className="quote">’</span>
+            <span className="brand">Clearo</span> helps you stay focused by detecting distractions on your desk in real time. 
+            Using your <span className="highlight">webcam</span>, <span className="brand">Clearo</span> identifies items like smartphones, 
+            books, and laptops, giving you a productivity score and actionable insights during your 
+            <span className="muted"> Pomodoro sessions.</span>
+          </p>
 
-        {/* Overview Section */}
-        <section className="overview">
-          <div className="overview-container">
-            <h2>Overview</h2>
-            <p>Lorem ipsum</p>
+          <div className="key-features">
+            <div className="key-features-container">
+              <p className="key-features-title">Key features</p>
+              
+              <div className="feature-item">
+                <p className="feature-title">Real-Time Object Detection</p>
+                <p className="feature-desc">Know what's on your desk instantly and spot potential distractions.</p>
+              </div>
+
+              <div className="feature-item">
+                <p className="feature-title">Pomodoro Integration</p>
+                <p className="feature-desc">Stay on track with customizable focus and break timers.</p>
+              </div>
+
+              <div className="feature-item">
+                <p className="feature-title">Productiviy Scoring & Insights</p>
+                <p className="feature-desc">Track your focus and get feedback to improve every sessions.</p>
+              </div>
+
+              <div className="feature-item">
+                <p className="feature-title">Visual & Interactive Dashboard</p>
+                <p className="feature-desc">See your desk, your score, and your streaks all in one place.</p>
+              </div>
+            </div>
+
+          <p className="overview-text overview-quotes">
+              <span className="quote-last">'</span>
+              See your focus. Boost your productivity.<br/>
+              Start your first session now!
+          </p>
+
           </div>
-        </section>
+          
+        </div>
+      </section>
 
-        {/* Contact Section */}
-        <section className="contact-us">
-          <div className="contact-us-container">
-            <h2>Contact Us</h2>
-            <p>Lorem ipsum</p>
-          </div>
-        </section>
-
-      </div>
+      {/* Contact Section */}
+      <section className="contact-us">
+        <div className="contact-us-container">
+          <h2>Contact Us</h2>
+          <p>Lorem ipsum</p>
+        </div>
+      </section>
 
     </>
   );
